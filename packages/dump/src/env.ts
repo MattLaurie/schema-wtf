c6942e7c472131decc6030b2306e3ff8bf12b6cb
Matt Laurie <MattLaurie@users.noreply.github.com>
2024-05-02 08:06:07 +1000

    misc: use zod.coerce for parsing env

--- packages/dump/src/env.ts
+++ packages/dump/src/env.ts
@@ -7,7 +7,7 @@ export const env = createEnv({
         DATABASE_NAME: z.string(),
         DATABASE_USERNAME: z.string(),
         DATABASE_PASSWORD: z.string(),
-        DATABASE_PORT: z.preprocess((val) => Number(val), z.number())
+        DATABASE_PORT: z.coerce.number()
     },
     runtimeEnv: process.env,
     emptyStringAsUndefined: true,
